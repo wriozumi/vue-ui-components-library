13c4e04e481595fd6bda82107e1200a0cc499c60
nazar shatmanaliev <tshatmanaliev@gmail.com>
2022-02-04 10:09:50 +0100

    add init project setup with sfc-init

---
+++ ort-core-components/dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { OrtCoreComponentsSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  OrtCoreComponentsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <ort-core-components-sample />
+  </div>
+</template>
